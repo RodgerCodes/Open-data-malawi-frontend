5cb0be2f2fbf2a2953c9f59d8d81643b4082c006
Rodger Kumwanje <51970312+RodgerCodes@users.noreply.github.com>
2023-11-27 10:12:04 +0200

    feat:migrated to vuejs + vite for the frontend

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import './assets/main.css'
 
-import { AppModule } from './app/app.module';
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
 
+const app = createApp(App)
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+app.use(router)
+
+app.mount('#app')
