83917bbae38a75e22b55829d271378fed1c715e5
Rodger Kumwanje <51970312+RodgerCodes@users.noreply.github.com>
2023-12-21 19:36:41 +0200

    feat: header

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,19 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+<script  lang="ts">
+import { RouterView } from 'vue-router'
+import HeaderComponent from '@/layout/header_component.vue';
+
+export default {
+  name: 'App',
+  components: {
+    RouterView,
+    HeaderComponent,
+  }
+}
 </script>
 
 <template>
   <header>
-
+     <HeaderComponent/>
   </header>
 
   <RouterView />
--- src/services/DatasetService.ts
+++ src/services/DatasetService.ts
@@ -2,6 +2,24 @@
 export class DataSetService  {
     apiUrl: string;
     constructor(apiUrl:string){
-        this.apiUrl = apiUrl;''
+        this.apiUrl = apiUrl;
     }
+    
+    async searchDataSetByName(query:string){
+
+    }
+
+    async getAllDatasets() {
+
+    }
+
+    async getSingleDataSet(id:number){
+
+    }
+
+    async createDataSet(data:any){
+
+    }
+
+
 }
